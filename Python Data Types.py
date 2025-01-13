8ee9c5ef7e5075946d1b813331be8b79bda699c7
LondonTrinity <7187@trinity.croydon.sch.uk>
2025-01-13 09:25:50 +0000

    Add files via upload

---
+++ Python Data Types.py
@@ -0,0 +1,44 @@
+#Data types
+
+
+
+#CRIBS
+
+Character = "y" "?" "2"
+
+real_Float = 12.333
+
+Integer = 428
+
+Boolean = True or False
+
+string = "hello123!!@"
+
+
+#It IS possible to do maths with floats and integers
+
+#It is NOT possible to do maths with strings or characters
+
+#e.g:
+
+num1 = 20 #int
+num2 = 30 #int
+total = num1 + num2
+print(total)#prints: 50
+
+num3 = 12.3 #float
+num4 = 93.2 #float
+total2 = num3 + num4
+print(total2)#prints: 105.5
+
+#You cannot add strings or characters
+#Instead, the add symbol glues them together
+#But we do not call it glueing, we call it CONCATENATION
+
+#e.g:
+
+num5 = "5" #string
+num6 = "88" #string
+total3 = num5 + num6
+print(total3) #prints: 588
+
